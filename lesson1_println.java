11d96549209dc1877a823be722875ee15d4c8501
AlekseyPlatonov <AlexPlatonov98@yandex.ru>
2023-04-25 21:50:16 +0300

    Update2

---
+++ lesson1_println.java
@@ -0,0 +1,14 @@
+package lesson1;
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("               РУБАИ");
+        System.out.println("Много лет размышлял я на жизни земной.");
+        System.out.println("Не понятного нет для меня под Луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                  О.Хайям");
+        System.out.println();
+    }
+    
+}
